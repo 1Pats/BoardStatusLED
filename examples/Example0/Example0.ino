f10db01903e09d982c1e6104a546d12546162891
1Pats <163280580+1Pats@users.noreply.github.com>
2026-03-03 05:17:57 +0200

    Add files via upload

---
+++ examples/Example0/Example0.ino
@@ -0,0 +1,14 @@
+/*
+ * Illustration of BoardStatusLED library application
+ * Absolute minimum
+ * 1Pats February 2026
+ * Code developed according to KISS principle
+ */
+
+#include "BoardStatusLED.h"
+
+void setup() {
+  LED(INFO);
+}
+
+void loop() { }
