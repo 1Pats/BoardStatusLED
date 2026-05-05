a0167018e750865fc412081fd63c1de70981b0c5
1Pats <163280580+1Pats@users.noreply.github.com>
2026-05-05 13:58:35 +0300

    Add files via upload

--- examples/Example1/Example1.ino
+++ examples/Example1/Example1.ino
@@ -3,10 +3,10 @@
  * Illustration of BoardStatusLED library usage
  * Sequentially changes the LED status
  * 1Pats February 2026
+ * Update - May 2026
+ * Please check #defines in ...\libraries\BoardStatusLed\src\BoardStatusLEDSetup.h
  * Code developed according to KISS principle
 */
-
-
 
 #include "BoardStatusLED.h"
 
@@ -15,8 +15,11 @@
 
 void setup() {
     Serial.begin(115200);
-    LED(CONNECTING);                                                           // initial status
-    // LEDUSER(15,15,15,100);                                                  // to update user pattern
+    while (!Serial) delay(10);
+    DP("\n************** BoardStatusLED Library Test ****************\n");
+    LED(USER);        
+    DP("USER\n");                                                             // initial status
+    LEDUSER(10,25,35,100);                                                    // update user pattern for next time
 }
 
 void loop() {
@@ -24,14 +27,28 @@ void loop() {
     if (millis() - ulLastChange < 10000) return;                               // change status every 10 seconds
     ulLastChange = millis();                                                   // time 
     static int iState = 0;                                                     // track current state
-    switch (iState++%8){                                                       // cycle through patterns
-        case 0:LED(CONNECTING);  DP("CONNECTING\n");   break;
-        case 1:LED(OK);          DP("OK\n");           break;
-        case 2:LED(INFO);        DP("INFO\n");         break;
-        case 3:LED(RAINBOW);     DP("RAINBOW\n");      break;
-        case 4:LED(WARNING);     DP("WARNING\n");      break;
-        case 5:LED(USER);        DP("USER\n");         break;
-        case 6:LED(ERROR);       DP("ERROR\n");        break;
-        case 7:LED(OFF);         DP("OFF\n");          break;
+    switch (iState++%20){                                                      // cycle through patterns
+        case 0: LED(CONNECTING);  DP("CONNECTING\n");   break;
+        case 1: LED(OK);          DP("OK\n");           break;
+        case 2: LED(INFO);        DP("INFO\n");         break;
+        case 3: LED(WORKING);     DP("WORKING\n");      break; 
+        case 4: LED(RAINBOW);     DP("RAINBOW\n");      break;
+        case 5: LED(ARMED);       DP("ARMED\n");        break; 
+        case 6: LED(RETRIEVING);  DP("RETRIEVNG\n");    break;
+        case 7: LED(WAITING);     DP("WAITING\n");      break;
+        case 8: LED(WARNING);     DP("WARNING\n");      break;
+        case 9: LED(USER);        DP("USER\n");         break;
+        case 10:LED(ERROR);       DP("ERROR\n");        break;
+        case 11:LED(RED);         DP("RED\n");          break;
+        case 12:LED(GREEN);       DP("GREEN\n");        break;                  
+        case 13:LED(BLUE);        DP("BLUE\n");         break;
+        case 14:LED(YELLOW);      DP("YELLOW\n");       break;
+        case 15:LED(PINK);        DP("PINK\n");         break;
+        case 16:LED(SKY);         DP("SKY\n");          break;  
+        case 17:LED(WHITE);       DP("WHITE\n");        break;
+        case 18:LED(OFF);         DP("OFF\n");          break;
+        case 19:                  DP("PAUSE\n\n\n");    break;                  // Pasue, then do it again
+       
+
     }  
 }
